dccd63ef378d4fcd540e8a450e91e1582f64ca79
pwiniar <winiarskipawel@gmail.com>
2016-12-07 23:52:01 +0100

    advent of code, scenario 7

--- advent-of-code-2016/src/main/java/day/seven/Main.java
+++ advent-of-code-2016/src/main/java/day/seven/Main.java
@@ -1,6 +1,5 @@
 package day.seven;
 
-import day.seven.analyzers.Analyzer;
 import day.seven.analyzers.Ssl;
 import day.seven.analyzers.Tls;
 
@@ -10,11 +9,8 @@ import day.seven.analyzers.Tls;
 public class Main {
 
     public static void main(String[] args) {
-        final String path = "advent-of-code-2016/src/main/java/day/seven/data/input";
-
-        Analyzer task1 = new Tls();
-        Analyzer task2 = new Ssl();
-
-        RunManager.execute(path, task1, task2);
+        RunManager.execute("advent-of-code-2016/src/main/java/day/seven/data/input",
+                new Tls(),
+                new Ssl());
     }
 }
